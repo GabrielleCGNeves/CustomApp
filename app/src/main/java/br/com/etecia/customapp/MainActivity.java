158e37668ab4a4d7e3c9e8a529398e2b814dfa5f
GabrielleCGNeves <gabriellecarvalhoneves@gmail.com>
2023-03-14 22:13:39 -0300

    :lipstick: added last element

--- app/src/main/java/br/com/etecia/customapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/customapp/MainActivity.java
@@ -2,15 +2,27 @@ package br.com.etecia.customapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    List<Filmes> listaFilmes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listaFilmes = new ArrayList<>();
 
+        listaFilmes.add(
+                new Filmes("Tudo em Todo Lugar",
+                        "Uma imigrante chinesa parte rumo a uma aventura onde, sozinha, precisará salvar o mundo, explorando outros universos e outras vidas que poderia ter vivido.",
+                        "Comédia",
+                        R.drawable.tudoaomesmotempo));
+        MyAdapter adapter = new MyAdapter(getApplicationContext(), listaFilmes);
 
     }
 }
--- app/src/main/java/br/com/etecia/customapp/MyAdapter.java
+++ app/src/main/java/br/com/etecia/customapp/MyAdapter.java
@@ -1,12 +1,30 @@
 package br.com.etecia.customapp;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+     private Context mContext;
+
+    public MyAdapter(Context mContext, List<Filmes> listFilmes) {
+        this.mContext = mContext;
+        this.listFilmes = listFilmes;
+    }
+
+    private List<Filmes> listFilmes;
+
+    public MyAdapter() {
+    }
 
     @NonNull
     @Override
@@ -24,13 +42,21 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         return 0;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView idTitulosFilmes;
+        ImageView idImagemFilmes;
+        CardView idCardView;
 
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            idTitulosFilmes = itemView.findViewById(R.id.idTitulosFilmes);
+            idImagemFilmes = itemView.findViewById(R.id.idImgFilmes);
+            idCardView = itemView.findViewById(R.id.idCardFilmes);
+
         }
     }
-    }
+}
 
 
